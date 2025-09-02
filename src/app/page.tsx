c10c5e7949afc2b3bed79887f3409815f9fc31ef
blackboxai-deploy <deploy@blackboxai.dev>
2025-09-03 01:50:22 +0530

    Add src/app/page.tsx

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import HelloWorld from "@/components/HelloWorld"
+
+export default function HomePage() {
+  return (
+    <div className="container mx-auto px-4 py-8">
+      <HelloWorld />
+    </div>
+  )
+}
